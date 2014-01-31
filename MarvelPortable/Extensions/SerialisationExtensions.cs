8ad132be6ebe22ee8ac2a7b48e11b72c5f3b965f
Scott Lovegrove <scottisafool@live.co.uk>
2014-01-31 18:21:24 +0000

    Added nuget packages and "standard" classes

---
+++ MarvelPortable/Extensions/SerialisationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MarvelPortable.Extensions
+{
+    internal static class SerialisationExtensions
+    {
+        internal static Task<TReturnType> DeserialiseAsync<TReturnType>(this string json)
+        {
+            return Task.Factory.StartNew(() => JsonConvert.DeserializeObject<TReturnType>(json));
+        }
+
+        internal static Task<string> SerialiseAsync(this object item)
+        {
+            return Task.Factory.StartNew(() => JsonConvert.SerializeObject(item));
+        }
+
+        internal static Task<string> SerialiseAsync(this object item, JsonConverter converter)
+        {
+            return Task.Factory.StartNew(() => JsonConvert.SerializeObject(item, converter));
+        }
+    }
+}
